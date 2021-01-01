61ab8d7c92c5272a9b719acde35d3ac1e69d78a0
KaiHOtw-1010 <51895176+KaiHOtw-1010@users.noreply.github.com>
2021-01-02 04:40:32 +0800

    add initial files of simon game

---
+++ game.js
@@ -0,0 +1,85 @@
+var gamePattern = [];
+
+var userClickedPattern = [];
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var started = false;
+var level = 0;
+
+$(".btn").click(function(){
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level "+level);
+    nextSequence();
+    started = true;
+  }
+});
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
+    console.log("success");
+  }
+  else {
+    console.log("wrong");
+    gameOver();
+    startOver();
+  }
+
+  if (currentLevel+1 == level) {
+    setTimeout(function(){
+      nextSequence();
+      userClickedPattern = [];
+    }, 1000);
+  }
+}
+
+function nextSequence() {
+  level += 1;
+  $("#level-title").text("Level "+level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
+}
+
+function gameOver() {
+  playSound("wrong");
+  $("body").addClass("game-over");
+  setTimeout(function(){
+    $("body").removeClass("game-over");
+  }, 200);
+  $("#level-title").text("Game Over, Press Any Key to Restart");
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  userClickedPattern = [];
+  started = false;
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/"+name+".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#"+currentColour).addClass("pressed");
+  setTimeout(function(){
+    $("#"+currentColour).removeClass("pressed");
+  }, 100);
+}
